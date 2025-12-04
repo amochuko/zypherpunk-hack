2008ee313060afa37447f16b18ef682365b74f04
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-04 03:03:33 +0100

    add faucet from address

--- apps/zcash-dev-sandbox/server/src/config.ts
+++ apps/zcash-dev-sandbox/server/src/config.ts
@@ -5,6 +5,7 @@ export const config = {
   PORT: process.env.PORT,
   API_KEY: process.env.API_KEY,
   FAUCET_AMOUNT: process.env.FAUCET_AMOUNT,
+  FAUCET_FROM_ADDRESS: String(process.env.FAUCET_FROM_ADDRESS),
   BASE_URL: process.env.BASE_URL,
   ZCASH_RPC_URL: String(process.env.ZCASH_RPC_URL),
   ZCASH_RPC_USER: String(process.env.ZCASH_RPC_USER),
