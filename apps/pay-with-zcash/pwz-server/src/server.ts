00b6e832c834c5882c5d0c76d262fdce63b2f459
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-05 18:26:55 +0100

    update with static middleware

--- apps/pay-with-zcash/pwz-server/src/server.ts
+++ apps/pay-with-zcash/pwz-server/src/server.ts
@@ -6,13 +6,13 @@ import { config } from "./config";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.static("public"));
 
 const modulesDir = path.join(__dirname, "modules");
 mountRouters(app, modulesDir);
 
-app.listen(config.port, () => {
+app.listen(config.port, "0.0.0.0", () => {
   console.log(
-    `Pay with Zcash gateway running on http://localhost:${config.port}/api`
+    `Pay with Zcash gateway running on http://0.0.0.0:${config.port}/api`
   );
 });
