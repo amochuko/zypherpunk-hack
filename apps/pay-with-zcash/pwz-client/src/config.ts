14f5fb1308847195a5212ea3a9e85f1a84bf755d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-05 14:09:40 +0100

    add embed url

--- apps/pay-with-zcash/pwz-client/src/config.ts
+++ apps/pay-with-zcash/pwz-client/src/config.ts
@@ -1,3 +1,4 @@
 export const config = {
-  VITE_API_HOST: import.meta.env.VITE_API_HOST,
+  VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  VITE_API_BASE_URL_EMBED_CODE: import.meta.env.VITE_API_BASE_URL_EMBED_CODE,
 };
