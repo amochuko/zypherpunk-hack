2009cc2220347069f4a3ad9e192a67fe669cc994
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-05 14:50:27 +0100

    parse to number

--- apps/pay-with-zcash/pwz-server/src/config.ts
+++ apps/pay-with-zcash/pwz-server/src/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT,
+  port: Number(process.env.PORT),
   webhookSecret: process.env.WEBHOOK_SECRET,
   baseUrl: process.env.BASE_URL,
 };
