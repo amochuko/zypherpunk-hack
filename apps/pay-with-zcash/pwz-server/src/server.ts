33058206f6edc6440c1b2f3ba8fa1a9c23715574
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-06 06:20:45 +0100

    add json middleware

--- apps/pay-with-zcash/pwz-server/src/server.ts
+++ apps/pay-with-zcash/pwz-server/src/server.ts
@@ -6,6 +6,7 @@ import { config } from "./config";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
 
 const modulesDir = path.join(__dirname, "modules");
